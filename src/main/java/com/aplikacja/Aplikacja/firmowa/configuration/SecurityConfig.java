e889a2440282b54182169fd24fd4068fb3593a81
Szarl3j <toomaszmoch@gmail.com>
2021-03-04 09:43:43 +0100

    dodanie podstawowej konfiguracji spring security

--- src/main/java/com/aplikacja/Aplikacja/firmowa/configuration/SecurityConfig.java
+++ src/main/java/com/aplikacja/Aplikacja/firmowa/configuration/SecurityConfig.java
@@ -1,5 +1,29 @@
 package com.aplikacja.Aplikacja.firmowa.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
 // in feature
-public class SecurityConfig {
+@Configuration
+@EnableWebSecurity(debug = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/", "/home").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll();
+    }
 }
