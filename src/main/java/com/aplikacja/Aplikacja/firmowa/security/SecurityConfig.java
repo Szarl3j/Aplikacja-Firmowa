dbd3f9a54ffe5ef5efe84a51be2f73190108c51c
Szarl3j <toomaszmoch@gmail.com>
2021-03-23 15:29:57 +0100

    Próboa rozwiązania problemu z uruchomieniem aplikacji.

--- src/main/java/com/aplikacja/Aplikacja/firmowa/security/SecurityConfig.java
+++ src/main/java/com/aplikacja/Aplikacja/firmowa/security/SecurityConfig.java
@@ -1,17 +1,44 @@
 package com.aplikacja.Aplikacja.firmowa.security;
 
+import com.aplikacja.Aplikacja.firmowa.security.JWebToken.AuthenticationTokenFilter;
+import com.aplikacja.Aplikacja.firmowa.security.service.UserDetailsServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-// in feature
 @Configuration
 @EnableWebSecurity(debug = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    UserDetailsServiceImpl userDetailsServiceImpl;
+
+//    @Autowired
+//    private AuthenticationEntryPointJWebToken unauthorize;
+
+    @Bean
+    public AuthenticationTokenFilter authenticationTokenFilter() {
+        return new AuthenticationTokenFilter();
+    }
+
+    @Override
+    public void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+        authenticationManagerBuilder.userDetailsService(userDetailsServiceImpl).passwordEncoder(
+                passwordEncoder());
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -27,6 +54,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .logout()
                 .permitAll();
     }
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
