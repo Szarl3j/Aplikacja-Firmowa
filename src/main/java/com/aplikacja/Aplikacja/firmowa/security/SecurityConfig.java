4658ae183e705abcab2a8a577f568fef35c33274
Szarl3j <toomaszmoch@gmail.com>
2021-03-31 11:21:27 +0200

    Create by hand Tables in DataBase: Documents, roles,user_roles and users Still working with postMapping and GetMapping. Application starts.

--- src/main/java/com/aplikacja/Aplikacja/firmowa/security/SecurityConfig.java
+++ src/main/java/com/aplikacja/Aplikacja/firmowa/security/SecurityConfig.java
@@ -51,7 +51,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/", "/home").permitAll()
+                .antMatchers("/user/authorize/register", "/home").permitAll()
                 .antMatchers("/user/authorize/register").permitAll()
                 .anyRequest().permitAll()
                 .and()
